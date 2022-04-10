d7763d7d1bdfa814a730acda3910b0436813687d
alexandre.romulo <alexandre.romulo@hotmail.com>
2022-04-10 18:49:23 -0300

    Adiconado o login do usuario

--- src/main/java/com/neri/bank/WebSecurityConfig.java
+++ src/main/java/com/neri/bank/WebSecurityConfig.java
@@ -1,9 +1,6 @@
 package com.neri.bank;
 
-
-
 import javax.sql.DataSource;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -18,59 +15,42 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.neri.bank.controller.UserReporsitory;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
-	
-	
+
+	@Autowired
+	private UserReporsitory userReporsitory;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()	
-				.anyRequest().authenticated()
-				.and().formLogin(form -> form.loginPage("/login")
-						.defaultSuccessUrl("/home",true)
-						.permitAll())
-				.logout(logout -> logout.logoutUrl("/logout"))
-				.csrf().disable();
+		http.authorizeRequests().anyRequest().authenticated().and()
+				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
+				.logout(logout -> logout.logoutUrl("/logout")).csrf().disable();
 	}
-	
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		
-		
-//		auth.jdbcAuthentication()
-//		.dataSource(dataSource)
-//		.passwordEncoder(encoder);
-//	
-			
-		
-	
-//		Criação de usuario		
-		UserDetails user =  User.builder()
-				  .username("neri")
-				  .password(encoder.encode("1234"))
-				  .roles("ADM")
-				  .build();
-		
-		auth.jdbcAuthentication()
-		.dataSource(dataSource)
-		.passwordEncoder(encoder)
-		.withUser(user);
-//			
-			
-		
-	}
-	
 
-	
-	
-	
+		com.neri.bank.model.User user1 = userReporsitory.findByUsername("neri");
+
+		if (user1.getEnabled().booleanValue()) {
+			auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder);
+		} else {
+
+//		Criação de usuario		
+			UserDetails user = User.builder().username("neri").password(encoder.encode("1234")).roles("ADM").build();
+
+			auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder).withUser(user);
+
+		}
+
+	}
+
 }
